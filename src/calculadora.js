28e3a5bcecf1ab7528ea6a06be77c76e5a7a05fc
Sostenes <sostenes.dev@gmail.com>
2020-05-07 23:56:43 -0300

    Criação do Teste unitário do CalculadoraService

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './calculadora.css';
 import {Jumbotron, Container, Row, Col,Button, Form} from 'react-bootstrap';
 
 function Calculadora() {
+  const [txtNumeros, setTxtNumeros] = useState('0');
+
+  function adicionarNumero(numero){
+    setTxtNumeros(txtNumeros+numero);
+  }
+
+  function definirOperacao(operacao){
+    setTxtNumeros(txtNumeros+operacao);
+  }
+
   return (
     <Container fluid style={{backgroundColor:"#ccc"}}>
        <Row className="justify-content-md-center">
@@ -22,33 +32,39 @@ function Calculadora() {
                   <Col xs="9">
                     <Form.Control type="text"
                       name="txtNumeros"
-                      class="text-right"
-                      readOnly="readOnly" />
+                      className="text-right"
+                      readOnly="readOnly" value={txtNumeros}/>
                     </Col>
                 </Row>
                 <Row>
-                  <Col><Button variant="light">7</Button></Col>
-                  <Col><Button variant="light">8</Button></Col>
-                  <Col><Button variant="light">9</Button></Col>
-                  <Col><Button variant="warning">/</Button></Col>
+                  <Col><Button 
+                      onClick={()=>adicionarNumero(7)}
+                    variant="light">7</Button></Col>
+                  <Col><Button variant="light"
+                  onClick={()=>adicionarNumero(8)}
+                  >8</Button></Col>
+                  <Col><Button variant="light"
+                  onClick={()=>adicionarNumero(9)}
+                  >9</Button></Col>
+                  <Col><Button variant="warning" onClick={()=>definirOperacao('/')} >/</Button></Col>
                 </Row>
                 <Row>
-                  <Col><Button variant="light">4</Button></Col>
-                  <Col><Button variant="light">5</Button></Col>
-                  <Col><Button variant="light">6</Button></Col>
-                  <Col><Button variant="warning">*</Button></Col>
+                  <Col><Button variant="light" onClick={()=>adicionarNumero(4)}>4</Button></Col>
+                  <Col><Button variant="light"onClick={()=>adicionarNumero(5)}>5</Button></Col>
+                  <Col><Button variant="light" onClick={()=>adicionarNumero(6)}>6</Button></Col>
+                  <Col><Button variant="warning" onClick={()=>definirOperacao('*')} >*</Button></Col>
                 </Row>
                 <Row>
-                  <Col><Button variant="light">1</Button></Col>
-                  <Col><Button variant="light">2</Button></Col>
-                  <Col><Button variant="light">3</Button></Col>
-                  <Col><Button variant="warning">-</Button></Col>
+                  <Col><Button variant="light" onClick={()=>adicionarNumero(1)}>1</Button></Col>
+                  <Col><Button variant="light" onClick={()=>adicionarNumero(2)}>2</Button></Col>
+                  <Col><Button variant="light" onClick={()=>adicionarNumero(3)}>3</Button></Col>
+                  <Col><Button variant="warning" onClick={()=>definirOperacao('-')} >-</Button></Col>
                 </Row>
                 <Row>
-                  <Col><Button variant="light">0</Button></Col>
-                  <Col><Button variant="light">.</Button></Col>
-                  <Col><Button variant="success">=</Button></Col>
-                  <Col><Button variant="warning">+</Button></Col>
+                  <Col><Button variant="light" onClick={()=>adicionarNumero(0)}>0</Button></Col>
+                  <Col><Button variant="light" onClick={()=>definirOperacao('.')} >.</Button></Col>
+                  <Col><Button variant="success" onClick={()=>definirOperacao('=')} >=</Button></Col>
+                  <Col><Button variant="warning" onClick={()=>definirOperacao('+')} >+</Button></Col>
                 </Row>
               </Container>
     </Jumbotron>
---
+++ src/calculadora.service.js
@@ -0,0 +1,12 @@
+function CalculadoraService(){
+    function calcular(numero1, numero2, operacao){
+        return 0;
+    }
+
+    return [calcular];
+    
+}
+
+
+
+export default CalculadoraService;
